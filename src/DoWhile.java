a28dee8cf77b50d5ab1a50ba3ac55a6bd7c97000
Caio Bello Zamana <caiobzm@gmail.com>
2023-05-06 07:06:57 -0300

    Att

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -2,8 +2,8 @@ public class DoWhile {
     public static void main(String[] args) {
         int i = 0;
         do {
-          System.out.println(i);
-          i++;
-        } while (i < 10);
+          System.out.println(i); //Sentença
+          i++; 
+        } while (i < 10); //Expressao
     }
 }
--- src/While.java
+++ src/While.java
@@ -2,8 +2,8 @@ public class While {
 
     public static void main(String[] args) {
         int i = 0;
-        while (i < 10) {
-            System.out.println(i);
+        while (i < 10) { //Expressao
+            System.out.println(i); //Sentenca
             i++;
           }
     }
